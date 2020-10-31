ee0544fbd9bd6dfa39a6bd295ccf61f0196b6410
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 15:13:32 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,67 @@
-# Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+row = [" ", " ", " "]
+# Position 1: Left 
+row[0]
+
+# Position 2: Middle 
+row[1]
+
+# Position 3: Right 
+row[2]
+
+# Move X to the Position 1, Left
+row[0] = "X"
+row #=> ["X", " ", " "]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
+
+# Move X to the Position 3, Right
+row[2] = "X"
+row #=> [" ", " ", "x"]
+
+row = [" ", " ", " "]
+# Position 4: Left 
+row[3]
+
+# Position 5: Middle 
+row[4]
+
+# Position 6: Right 
+row[5]
+
+# Move X to the Position 4, Left
+row[3] = "X"
+row #=> ["X", " ", " "]
+
+# Move X to the Position 5, Middle
+row[4] = "X"
+row #=> [" ", "X", " "]
+
+# Move X to the Position 6, Right
+row[5] = "X"
+row #=> [" ", " ", "x"]
+
+row = [" ", " ", " "]
+# Position 1: Left 
+row[6]
+
+# Position 2: Middle 
+row[7]
+
+# Position 3: Right 
+row[8]
+
+# Move X to the Position 1, Left
+row[6] = "X"
+row #=> ["X", " ", " "]
+
+# Move X to the Position 2, Middle
+row[7] = "X"
+row #=> [" ", "X", " "]
+
+# Move X to the Position 3, Right
+row[8] = "X"
+row #=> [" ", " ", "x"]
